c7f4101cd4985f37c9598c83db378cbdc011344f
EUR\SNR <snezhanaribarska@softwareag.com>
2022-02-11 17:27:15 +0200

    Revert "implement POS-tagging"
    This reverts commit 5a90b58fc860256227a01a9a49a45386c1cd844e.

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 import re
-import spacy
 from nltk.tokenize import word_tokenize
 
 sentences = ["Каква е сложността на алгоритъма ЙАК?", "От къде започва тъсенето на термин в дърво за търсене?"]
@@ -24,6 +23,4 @@ for cleaned_sentence in cleaned_sentences :
 		if token.lower() not in stop_words:
 			filtered_sentence.append(token)
 	print(filtered_sentence)
-    
-
 
