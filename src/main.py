77c669dbe99a28386fd58c244541b3cd0cc29b03
astoeff <antoni.1998@abv.bg>
2022-02-12 00:42:07 +0200

    add dummy finding answer in given closest document

--- src/main.py
+++ src/main.py
@@ -21,24 +21,46 @@ def get_stop_words():
 	stop_words = stop_words_txt.split("\n")
 	return stop_words
 
-def get_cleaned_questions(questions):
-	cleaned_questions = []
-	for question in questions:
-		cleaned_question = re.sub(r'[^\w\s\-]', '', question)
-		cleaned_questions.append(cleaned_question)
-	return cleaned_questions
+def clean_question(question):
+	return re.sub(r'[^\w\s\-]', '', question)
 
-def get_filtered_questions(questions):
-	filtered_questions = []
-	stop_words = get_stop_words()
-	for question in questions: 
-		tokens = word_tokenize(question)
-		filtered_question = []
-		for token in tokens: 
-			if token.lower() not in stop_words:
-				filtered_question.append(token)
-		filtered_questions.append(filtered_question)
-	return filtered_questions
+# def get_cleaned_questions(questions):
+# 	cleaned_questions = []
+# 	for question in questions:
+# 		cleaned_question = re.sub(r'[^\w\s\-]', '', question)
+# 		cleaned_questions.append(cleaned_question)
+# 	return cleaned_questions
+
+# def get_filtered_questions(questions):
+# 	filtered_questions = []
+# 	stop_words = get_stop_words()
+# 	for question in questions:
+# 		tokens = word_tokenize(question)
+# 		filtered_question = []
+# 		for token in tokens:
+# 			if token.lower() not in stop_words:
+# 				filtered_question.append(token)
+# 		filtered_questions.append(filtered_question)
+# 	return filtered_questions
+
+def filter_question_words(question, stop_words):
+	tokens = word_tokenize(question)
+	filtered_question_words = []
+	for token in tokens:
+		if token.lower() not in stop_words:
+			filtered_question_words.append(token)
+	return filtered_question_words
+
+def print_question_with_answers(question, answers, question_number):
+	print("Въпрос %s: %s" % (question_number, question))
+	print("A) %s" % answers[0])
+	print("B) %s" % answers[1])
+	print("C) %s" % answers[2])
+	print("D) %s" % answers[3])
+	print("-------------------------------------------------")
+
+def check_if_answer_appears_in_document_words(answer, document):
+	return answer in document
 
 if __name__ == "__main__":
 	f = io.open("IR_lecture_notes.txt", mode="r", encoding="utf-8")
@@ -46,28 +68,54 @@ if __name__ == "__main__":
 
 	l = re.split(r'\b[0-9][0-9]?\s[а-яА-Я\s,]+\n', s)
 
-	file_paths = glob.glob("../resources/files/text*.txt")
-	documents = []
-	for path in file_paths:
-		file = open(path, "r", encoding="utf-8")
-		file_txt = file.read()
-		documents.append(file_txt)
+	# file_paths = glob.glob("../resources/files/text*.txt")
+	# documents = []
+	# for path in file_paths:
+	# 	file = open(path, "r", encoding="utf-8")
+	# 	file_txt = file.read()
+	# 	documents.append(file_txt)
 
 	json_file = open("../resources/questions.json", "r", encoding="utf-8")
 	data = json.load(json_file)
 	json_file.close()
 
 	questions = []
-	answers = []
+	stop_words = get_stop_words()
+	# answers = []
+	c = 1
 	for obj in data:
-		questions.append(obj)
-		answers.append(data[obj])
-		
-	cleaned_questions = get_cleaned_questions(questions)
-	filtered_questions = get_filtered_questions(cleaned_questions)
+		answers = data[obj]
+		print_question_with_answers(obj, answers, c)
+		cleaned_question = clean_question(obj)
+		filtered_question_words = filter_question_words(cleaned_question, stop_words)
 
-	print(filtered_questions)
-	print(" ".join(filtered_questions[3]))
-	closest_document = get_closest_document(l, " ".join(filtered_questions[3]))
-	print(closest_document)
+		# print(filtered_question_words)
+		# print(" ".join(filtered_question_words))
+		closest_document = get_closest_document(l, " ".join(filtered_question_words))
+		# closest_document_words = filter_question_words(closest_document, stop_words)
+		closest_document = closest_document.replace('.', '')
+		closest_document_words = closest_document.split(" ")
+
+		answers_found = []
+		for answer in answers:
+			if check_if_answer_appears_in_document_words(answer, closest_document):
+				answers_found.append(answer)
+
+		if answers_found:
+			if len(answers_found) > 1:
+				answers_found_as_whole_words = []
+				for answer_found in answers_found:
+					if check_if_answer_appears_in_document_words(answer_found, closest_document_words):
+						answers_found_as_whole_words.append(answer_found)
+				if answers_found_as_whole_words:
+					if len(answers_found_as_whole_words) > 1:
+						print("More than one answers found")
+					else:
+						print(answers_found_as_whole_words[0])
+			else:
+				print(answers_found[0])
+		else:
+			print("No answer found!")
+		print("\n\n")
+		c += 1
 
